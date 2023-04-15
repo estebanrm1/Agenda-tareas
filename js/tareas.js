78b238e774c9fe2dc661763e4b036b926f4e7ab7
estebanrm1 <solsel90@gmail.com>
2023-04-15 20:33:50 -0300

    Se modifico el maquetado segun la logica

--- js/tareas.js
+++ js/tareas.js
@@ -1,1 +1,12 @@
-let listaDeTareas
+let listaDeTareas = document.getElementById('listaDeTareas'),
+tareaInput = document.getElementById('tareaInput'),
+buttonAgregar = document.getElementById('buttonAgregar'),
+buttonEliminar = document.getElementById('buttonEliminar');
+
+const agregarTarea = function(){};
+const eliminarTarea = function(){ alert('Eliminar tarea')};
+
+
+buttonAgregar.addEventListener("click", agregarTarea);
+buttonEliminar.addEventListener("click", eliminarTarea);
+
