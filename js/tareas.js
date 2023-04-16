27986382d4a33cf15acfffdfbae9dc173562cdce
estebanrm1 <solsel90@gmail.com>
2023-04-15 21:28:52 -0300

    Se agrego la funcion agregar tarea para click y tecla enter

--- js/tareas.js
+++ js/tareas.js
@@ -1,12 +1,32 @@
-let listaDeTareas = document.getElementById('listaDeTareas'),
-tareaInput = document.getElementById('tareaInput'),
-buttonAgregar = document.getElementById('buttonAgregar'),
-buttonEliminar = document.getElementById('buttonEliminar');
+const fecha = document.querySelector('#fecha');
+const lista = document.querySelector('#listaDeTareas');
+const input = document.querySelector('#tareaInput');
+const botonAgregar = document.querySelector('#agregar');
 
-const agregarTarea = function(){};
-const eliminarTarea = function(){ alert('Eliminar tarea')};
+function agregarTarea (tarea){
+    const elemento = `
+    <li id="elemento">
+    <i class="bi bi-circle" data="realizado" id="0"></i>
+    <p class="text">${tarea}</p>
+    <i class="bi bi-trash" data="eliminado" id="0"></i>
+    </li>`
+    lista.insertAdjacentHTML("beforeend", elemento)
+}
 
+botonAgregar.addEventListener("click", ()=> {
+    const tarea = input.value
+    if(tarea){
+        agregarTarea(tarea)
+    }
+    input.value = ''
+})
 
-buttonAgregar.addEventListener("click", agregarTarea);
-buttonEliminar.addEventListener("click", eliminarTarea);
-
+document.addEventListener("keyup", function(event){
+    if(event.key == 'Enter'){
+        const tarea = input.value
+        if(tarea){
+            agregarTarea(tarea)
+        }
+    input.value=''
+    }
+})
